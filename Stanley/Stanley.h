3ea3149069a2c4c68d8d873e6d47be23532ef840
William Towe <willbur1984@gmail.com>
2017-03-07 13:35:07 -0500

    added targets

---
+++ Stanley/Stanley.h
@@ -0,0 +1,19 @@
+//
+//  Stanley.h
+//  Stanley
+//
+//  Created by William Towe on 3/7/17.
+//  Copyright © 2017 Kosoku Interactive, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stanley.
+FOUNDATION_EXPORT double StanleyVersionNumber;
+
+//! Project version string for Stanley.
+FOUNDATION_EXPORT const unsigned char StanleyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stanley/PublicHeader.h>
+
+
