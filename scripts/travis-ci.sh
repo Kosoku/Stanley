542cbdc08b90db65329585f89a9c59733e3eb501
William Towe <willbur1984@gmail.com>
2017-03-08 19:34:32 -0500

    use separate shell script for travis-ci

---
+++ scripts/travis-ci.sh
@@ -0,0 +1,3 @@
+xcodebuild test -project Stanley.xcodeproj -scheme StanleyTests-iOS -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 7 Plus,OS=10.0" ONLY_ACTIVE_ARCH=NO | xcpretty
+xcodebuild test -project Stanley.xcodeproj -scheme StanleyTests-tvOS -sdk appletvsimulator -destination "platform=tvOS Simulator,name=Apple TV 1080p,OS=10.0" ONLY_ACTIVE_ARCH=NO | xcpretty
+xcodebuild test -project Stanley.xcodeproj -scheme StanleyTests-macOS -sdk macosx ONLY_ACTIVE_ARCH=NO | xcpretty
