b9166068b64c74d719203f986dbc600dfd5c036c
William Towe <willbur1984@gmail.com>
2017-04-19 22:04:44 -0400

    comments

--- Stanley/KSTFileWatcher.h
+++ Stanley/KSTFileWatcher.h
@@ -17,28 +17,89 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ Options mask describing the cases where watcher events should be generated. See dispatch_source_vnode_flags_t for more information.
+ */
 typedef NS_OPTIONS(unsigned long, KSTFileWatcherFlags) {
+    /**
+     See DISPATCH_VNODE_DELETE for more information.
+     */
     KSTFileWatcherFlagsDelete = DISPATCH_VNODE_DELETE,
+    /**
+     See DISPATCH_VNODE_WRITE for more information.
+     */
     KSTFileWatcherFlagsWrite = DISPATCH_VNODE_WRITE,
+    /**
+     See DISPATCH_VNODE_EXTEND for more information.
+     */
     KSTFileWatcherFlagsExtend = DISPATCH_VNODE_EXTEND,
+    /**
+     See DISPATCH_VNODE_ATTRIB for more information.
+     */
     KSTFileWatcherFlagsAttrib = DISPATCH_VNODE_ATTRIB,
+    /**
+     See DISPATCH_VNODE_LINK for more information.
+     */
     KSTFileWatcherFlagsLink = DISPATCH_VNODE_LINK,
+    /**
+     See DISPATCH_VNODE_RENAME for more information.
+     */
     KSTFileWatcherFlagsRename = DISPATCH_VNODE_RENAME,
+    /**
+     See DISPATCH_VNODE_REVOKE for more information.
+     */
     KSTFileWatcherFlagsRevoke = DISPATCH_VNODE_REVOKE,
+    /**
+     See DISPATCH_VNODE_FUNLOCK for more information.
+     */
     KSTFileWatcherFlagsUnlock = DISPATCH_VNODE_FUNLOCK,
+    /**
+     Generate watcher events for all cases.
+     */
     KSTFileWatcherFlagsAll = KSTFileWatcherFlagsDelete|KSTFileWatcherFlagsWrite|KSTFileWatcherFlagsExtend|KSTFileWatcherFlagsAttrib|KSTFileWatcherFlagsLink|KSTFileWatcherFlagsRename|KSTFileWatcherFlagsRevoke|KSTFileWatcherFlagsUnlock
 };
 
 @class KSTFileWatcher;
 
+/**
+ Block that is invoked whenever a watcher event is generated.
+ 
+ @param fileWatcher The file watcher
+ @param URL The URL of the item that generated the event
+ @param flags The flags describing the type of event
+ */
 typedef void(^KSTFileWatcherBlock)(KSTFileWatcher *fileWatcher, NSURL *URL, KSTFileWatcherFlags flags);
 
+/**
+ KSTFileWatcher wraps the GCD APIs related to the kqueue file notification mechanism.
+ */
 @interface KSTFileWatcher : NSObject
 
+/**
+ Get the file URLs that are being watched.
+ */
 @property (readonly,copy,nonatomic) NSArray<NSURL *> *URLs;
+/**
+ Get the flags passed in during creation.
+ */
 @property (readonly,assign,nonatomic) KSTFileWatcherFlags flags;
 
+/**
+ Returns `[self initWithURLs:URLs flags:KSTFileWatcherFlagsAll block:block]`.
+ 
+ @param URLs The file/directory URLs to watch
+ @param block The block to invoke when an event occurs
+ @return The initialized instance
+ */
 - (instancetype)initWithURLs:(NSArray<NSURL *> *)URLs block:(KSTFileWatcherBlock)block;
+/**
+ Creates and returns an instance of the receiver to watch the provided file/directory URLs and invoke the provided *block* whenever an event occurs. The provided *block* is always invoked on a private background queue.
+ 
+ @param URLs The file/directory URLs to watch
+ @param flags The flags determining when to generate events
+ @param block The block to invoke when an event occurs
+ @return The initialized instance
+ */
 - (instancetype)initWithURLs:(NSArray<NSURL *> *)URLs flags:(KSTFileWatcherFlags)flags block:(KSTFileWatcherBlock)block NS_DESIGNATED_INITIALIZER;
 - (instancetype)init NS_UNAVAILABLE;
 
--- Stanley/macOS/KSTDirectoryWatcher.h
+++ Stanley/macOS/KSTDirectoryWatcher.h
@@ -18,56 +18,189 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ Options mask describing the possible values passed when creating a KSTDirectoryWatcher instance. See FSEventStreamCreateFlags for more information.
+ */
 typedef NS_OPTIONS(UInt32, KSTDirectoryWatcherCreateFlags) {
+    /**
+     See kFSEventStreamCreateFlagNone for more information.
+     */
     KSTDirectoryWatcherCreateFlagsNone = kFSEventStreamCreateFlagNone,
+    /**
+     See kFSEventStreamCreateFlagNoDefer for more information.
+     */
     KSTDirectoryWatcherCreateFlagsNoDefer = kFSEventStreamCreateFlagNoDefer,
+    /**
+     See kFSEventStreamCreateFlagIgnoreSelf for more information.
+     */
     KSTDirectoryWatcherCreateFlagsIgnoreSelf = kFSEventStreamCreateFlagIgnoreSelf,
+    /**
+     See kFSEventStreamCreateFlagFileEvents for more information.
+     */
     KSTDirectoryWatcherCreateFlagsFlagFileEvents = kFSEventStreamCreateFlagFileEvents,
+    /**
+     See kFSEventStreamCreateFlagMarkSelf for more information.
+     */
     KSTDirectoryWatcherCreateFlagsMarkSelf = kFSEventStreamCreateFlagMarkSelf
 };
 
+/**
+ Options mask describing the possible values passed to the block that is invoked when watcher events happen. See FSEventStreamEventFlags for more information.
+ */
 typedef NS_OPTIONS(UInt32, KSTDirectoryWatcherEventFlags) {
+    /**
+     See kFSEventStreamEventFlagNone for more information.
+     */
     KSTDirectoryWatcherEventFlagsNone = kFSEventStreamEventFlagNone,
+    /**
+     See kFSEventStreamEventFlagMustScanSubDirs for more information.
+     */
     KSTDirectoryWatcherEventFlagsMustScanSubDirs = kFSEventStreamEventFlagMustScanSubDirs,
+    /**
+     See kFSEventStreamEventFlagUserDropped for more information.
+     */
     KSTDirectoryWatcherEventFlagsUserDropped = kFSEventStreamEventFlagUserDropped,
+    /**
+     See kFSEventStreamEventFlagKernelDropped for more information.
+     */
     KSTDirectoryWatcherEventFlagsKernelDropped = kFSEventStreamEventFlagKernelDropped,
+    /**
+     See kFSEventStreamEventFlagEventIdsWrapped for more information.
+     */
     KSTDirectoryWatcherEventFlagsEventIDsWrapped = kFSEventStreamEventFlagEventIdsWrapped,
+    /**
+     See kFSEventStreamEventFlagHistoryDone for more information.
+     */
     KSTDirectoryWatcherEventFlagsHistoryDone = kFSEventStreamEventFlagHistoryDone,
+    /**
+     See kFSEventStreamEventFlagRootChanged for more information.
+     */
     KSTDirectoryWatcherEventFlagsRootChanged = kFSEventStreamEventFlagRootChanged,
+    /**
+     See kFSEventStreamEventFlagMount for more information.
+     */
     KSTDirectoryWatcherEventFlagsMount = kFSEventStreamEventFlagMount,
+    /**
+     See kFSEventStreamEventFlagUnmount for more information.
+     */
     KSTDirectoryWatcherEventFlagsUnmount = kFSEventStreamEventFlagUnmount,
+    /**
+     See kFSEventStreamEventFlagItemCreated for more information.
+     */
     KSTDirectoryWatcherEventFlagsItemCreated = kFSEventStreamEventFlagItemCreated,
+    /**
+     See kFSEventStreamEventFlagItemRemoved for more information.
+     */
     KSTDirectoryWatcherEventFlagsItemRemoved = kFSEventStreamEventFlagItemRemoved,
+    /**
+     See kFSEventStreamEventFlagItemInodeMetaMod for more information.
+     */
     KSTDirectoryWatcherEventFlagsItemInodeMetaMod = kFSEventStreamEventFlagItemInodeMetaMod,
+    /**
+     See kFSEventStreamEventFlagItemRenamed for more information.
+     */
     KSTDirectoryWatcherEventFlagsItemRenamed = kFSEventStreamEventFlagItemRenamed,
+    /**
+     See kFSEventStreamEventFlagItemModified for more information.
+     */
     KSTDirectoryWatcherEventFlagsItemModified = kFSEventStreamEventFlagItemModified,
+    /**
+     See kFSEventStreamEventFlagItemFinderInfoMod for more information.
+     */
     KSTDirectoryWatcherEventFlagsFinderInfoMod = kFSEventStreamEventFlagItemFinderInfoMod,
+    /**
+     See kFSEventStreamEventFlagItemChangeOwner for more information.
+     */
     KSTDirectoryWatcherEventFlagsItemChangeOwner = kFSEventStreamEventFlagItemChangeOwner,
+    /**
+     See kFSEventStreamEventFlagItemXattrMod for more information.
+     */
     KSTDirectoryWatcherEventFlagsItemXattrMod = kFSEventStreamEventFlagItemXattrMod,
+    /**
+     See kFSEventStreamEventFlagItemIsFile for more information.
+     */
     KSTDirectoryWatcherEventFlagsItemIsFile = kFSEventStreamEventFlagItemIsFile,
+    /**
+     See kFSEventStreamEventFlagItemIsDir for more information.
+     */
     KSTDirectoryWatcherEventFlagsItemIsDir = kFSEventStreamEventFlagItemIsDir,
+    /**
+     See kFSEventStreamEventFlagItemIsSymlink for more information.
+     */
     KSTDirectoryWatcherEventFlagsItemIsSymlink = kFSEventStreamEventFlagItemIsSymlink,
+    /**
+     See kFSEventStreamEventFlagOwnEvent for more information.
+     */
     KSTDirectoryWatcherEventFlagsOwnEvent = kFSEventStreamEventFlagOwnEvent,
+    /**
+     See kFSEventStreamEventFlagItemIsHardlink for more information.
+     */
     KSTDirectoryWatcherEventFlagsItemIsHardLink = kFSEventStreamEventFlagItemIsHardlink,
+    /**
+     See kFSEventStreamEventFlagItemIsLastHardlink for more information.
+     */
     KSTDirectoryWatcherEventFlagsItemIsLastHardLink = kFSEventStreamEventFlagItemIsLastHardlink
 };
 
+/**
+ Typedef for the event ID that is passed in the block when watcher events happen.
+ */
 typedef FSEventStreamEventId KSTDirectoryWatcherEventID;
 
 @class KSTDirectoryWatcher;
 
+/**
+ Block that is invoked when a watcher event happens.
+ 
+ @param directoryWatcher The directory watcher that generated the event
+ @param eventID The ID of the event
+ @param flags The flags describing the event
+ @param URL The URL pointing to the file/directory that generated the event
+ */
 typedef void(^KSTDirectoryWatcherBlock)(KSTDirectoryWatcher *directoryWatcher, KSTDirectoryWatcherEventID eventID, KSTDirectoryWatcherEventFlags flags, NSURL *URL);
 
+/**
+ KSTDirectoryWatcher wraps the FSEvents APIs and allows you to watch a collection of directory URLs and invoke the provided block when an event takes place.
+ */
 @interface KSTDirectoryWatcher : NSObject
 
+/**
+ Get the directory URLs that are being watched.
+ */
 @property (readonly,copy,nonatomic) NSArray<NSURL *> *URLs;
+/**
+ Get the flags that were passed in on creation.
+ 
+ @see KSTDirectoryWatcherCreateFlags
+ */
 @property (readonly,assign,nonatomic) KSTDirectoryWatcherCreateFlags flags;
 
+/**
+ Returns `[self initWithURLs:URLs flags:KSTDirectoryWatcherCreateFlagsNone block:block]`.
+ 
+ @param URLs The directory URLs to watch
+ @param block The block to invoke whenever an event occurs
+ @return The initialized instance
+ */
 - (instancetype)initWithURLs:(NSArray<NSURL *> *)URLs block:(KSTDirectoryWatcherBlock)block;
+/**
+ Creates and returns an instance of the receiver to watch the provided directory URLs and invoke the provided *block* whenever an event occurs. The *block* will always be invoked on a private background queue.
+ 
+ @param URLs The directory URLs to watch
+ @param flags The flags affecting watch behavior
+ @param block The block to invoke whenever an event occurs
+ @return The initialized instance
+ */
 - (instancetype)initWithURLs:(NSArray<NSURL *> *)URLs flags:(KSTDirectoryWatcherCreateFlags)flags block:(KSTDirectoryWatcherBlock)block NS_DESIGNATED_INITIALIZER;
 - (instancetype)init NS_UNAVAILABLE;
 
+/**
+ Start watching the directory URLs.
+ */
 - (void)startWatchingURLs;
+/**
+ Stop watching the directory URLs.
+ */
 - (void)stopWatchingURLs;
 
 @end
