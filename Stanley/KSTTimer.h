100994ca915cca3e96d0fef628b153a6e60646a9
William Towe <willbur1984@gmail.com>
2018-02-01 10:10:37 -0500

    comments

--- Stanley/KSTTimer.h
+++ Stanley/KSTTimer.h
@@ -19,19 +19,63 @@ NS_ASSUME_NONNULL_BEGIN
 
 @class KSTTimer;
 
+/**
+ Block that is invoked when a KSTTimer instance fires. The timer itself is passed as the only argument.
+ 
+ @param timer The timer that fired
+ */
 typedef void(^KSTTimerBlock)(KSTTimer *timer);
 
+/**
+ KSTTimer is a replacement for NSTimer that supports blocks and does not retain its target.
+ */
 @interface KSTTimer : NSObject
 
+/**
+ Returns whether the timer is valid and capable of firing. This will be NO if the timer has been invalidated using the invalidate method.
+ */
 @property (readonly,getter=isValid) BOOL valid;
+/**
+ Returns the userInfo object that passed in on initialization.
+ */
 @property (readonly,strong,nonatomic,nullable) id userInfo;
+/**
+ Sets the allowable tolerance for the timer, meaning how much the system can adjust the fire time of the timer to optimize energy usage.
+ */
 @property (assign) NSTimeInterval tolerance;
 
+/**
+ Creates and returns a timer using initWithTimeInterval:target:selector:userInfo:repeats:queue: and calls schedule on it.
+ */
 + (instancetype)scheduledTimerWithTimeInterval:(NSTimeInterval)timeInterval target:(id)target selector:(SEL)selector userInfo:(nullable id)userInfo repeats:(BOOL)repeats queue:(nullable dispatch_queue_t)queue;
+/**
+ Creates and returns a timer using initWithTimeInterval:block:userInfo:repeats:queue: and calls schedule on it.
+ */
 + (instancetype)scheduledTimerWithTimeInterval:(NSTimeInterval)timeInterval block:(KSTTimerBlock)block userInfo:(nullable id)userInfo repeats:(BOOL)repeats queue:(nullable dispatch_queue_t)queue;
 
-- (instancetype)initWithTimeInterval:(NSTimeInterval)timeInterval target:(id)target selector:(SEL)selector userInfo:(nullable id)userInfo repeats:(BOOL)repeats queue:(nullable dispatch_queue_t)queue;
-- (instancetype)initWithTimeInterval:(NSTimeInterval)timeInterval block:(KSTTimerBlock)block userInfo:(id)userInfo repeats:(BOOL)repeats queue:(nullable dispatch_queue_t)queue;
+/**
+ Creates and returns a timer that will invoke the provided selector on target when it fires.
+ 
+ @param timeInterval The interval at which the timer should fire
+ @param target The target on which to invoke selector
+ @param selector The selector to invoke on target
+ @param userInfo The user info to associate with the timer
+ @param repeats Whether the timer should repeat
+ @param queue The queue on which to fire the timer, pass nil to use the main queue
+ @return The initialized timer
+ */
+- (instancetype)initWithTimeInterval:(NSTimeInterval)timeInterval target:(id)target selector:(SEL)selector userInfo:(nullable id)userInfo repeats:(BOOL)repeats queue:(nullable dispatch_queue_t)queue NS_DESIGNATED_INITIALIZER;
+/**
+ Creates and returns a timer that will invoke a block when it fires.
+ 
+ @param timeInterval The interval at which the timer should fire
+ @param block The block to invoke when the timer fires
+ @param userInfo The user info to associate with the timer
+ @param repeats Whether the timer should repeat
+ @param queue The queue on which to fire the timer, pass nil to use the main queue
+ @return The initialized timer
+ */
+- (instancetype)initWithTimeInterval:(NSTimeInterval)timeInterval block:(KSTTimerBlock)block userInfo:(id)userInfo repeats:(BOOL)repeats queue:(nullable dispatch_queue_t)queue NS_DESIGNATED_INITIALIZER;
 
 - (instancetype)init NS_UNAVAILABLE;
 + (instancetype)new NS_UNAVAILABLE;
