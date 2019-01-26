47df089a478fcc6f01d4a80f31dc068f3f259e0a
William Towe <willbur1984@gmail.com>
2019-01-25 19:29:58 -0500

    update travis-ci info and test invocations

--- scripts/travis-ci.sh
+++ scripts/travis-ci.sh
@@ -1,3 +1,10 @@
-xcodebuild test -project Stanley.xcodeproj -scheme StanleyTests-iOS -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 7 Plus,OS=10.0" ONLY_ACTIVE_ARCH=NO GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty
-xcodebuild test -project Stanley.xcodeproj -scheme StanleyTests-tvOS -sdk appletvsimulator -destination "platform=tvOS Simulator,name=Apple TV 1080p,OS=10.0" ONLY_ACTIVE_ARCH=NO GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty
+#!/bin/sh
+
+# iOS tests
+xcodebuild test -project Stanley.xcodeproj -scheme StanleyTests-iOS -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 8 Plus,OS=latest" ONLY_ACTIVE_ARCH=NO GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty
+
+# tvOS tests
+xcodebuild test -project Stanley.xcodeproj -scheme StanleyTests-tvOS -sdk appletvsimulator -destination "platform=tvOS Simulator,name=Apple TV,OS=10.0" ONLY_ACTIVE_ARCH=NO GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty
+
+# macOS tests
 xcodebuild test -project Stanley.xcodeproj -scheme StanleyTests-macOS -sdk macosx ONLY_ACTIVE_ARCH=NO GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES | xcpretty
